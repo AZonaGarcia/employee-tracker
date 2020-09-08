6c84cc2d8c81db75b910e91ce8b34ea141719de1
Bonus Mom <AZonaGarcia@aol.com>
2020-09-08 01:28:46 -0400

    Created inquirer prompts

--- employeeTrackerReadData.js
+++ employeeTrackerReadData.js
@@ -1,22 +1,57 @@
 var mysql = require("mysql");
-var inquirer = require("inquirer")
+var inquirer = require("inquirer");
+const Choices = require("inquirer/lib/objects/choices");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "",
-  database: "employeeTracker_db"
+    // Your password
+    password: "",
+    database: "employeeTracker_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     createProduct();
-  });
+});
+
+inquirer.
+    prompt(
+        {
+            type: "input",
+            message: "What would you like to do?",
+            name: "task",
+            choices: [
+                "View employees",
+                "View departments",
+                "View employee roles",
+                "Add employee",
+                "Add employee department",
+                "Add employee role",
+                "Delete employee",
+                "Update employee roles"
+            ]
+        },
+        {
+            type: "input",
+            message: "Employee Name",
+            name: "empName"
+        },
+        {
+            type: "input",
+            message: "Employee Department",
+            name: "empDepartment"
+        },
+        {
+            type: "input",
+            message: "Employee Role",
+            name: "empRole"
+        }
+    )
--- seed.sql
+++ seed.sql
@@ -1,6 +1,24 @@
+USE employeeeTracker_db;
+
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 ('Macy', 'Vaugn', 1031, 0925),
 ('Mel', 'Vera', 1031, 1115),
-('Maggie', 'Vera', 1031, 1011),
-('Harry', 'Greenwood', 1231, 0702);
+('Maggie', 'Vera', 1031, 1),
+('Harry', 'Greenwood', 1231, 2);
+
+DELETE FROM employee;
+
+SELECT * FROM employee;
+
+ALTER TABLE employee AUTO_INCREMENT = 1;
+
+SELECT *
+FROM employee mi1, employee mi2
+WHERE mi1.id = mi2.manager_id;
+
+UPDATE employee 
+SET id = 2
+WHERE first_name = 'Mel';
+
+
